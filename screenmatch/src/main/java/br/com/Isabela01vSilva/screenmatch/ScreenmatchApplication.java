fe868df0dc8543d37889a6998d32e6f090e1dd4f
isabela.vsilva@bandtec.com.br <isabela.vsilva@bandtec.com.br>
2024-08-14 19:25:02 -0300

    Projeto Springboot sem Web e JsonAlias

---
+++ screenmatch/src/main/java/br/com/Isabela01vSilva/screenmatch/ScreenmatchApplication.java
@@ -0,0 +1,24 @@
+package br.com.Isabela01vSilva.screenmatch;
+
+import br.com.Isabela01vSilva.screenmatch.service.ConsumoApi;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenmatchApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenmatchApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		ConsumoApi consumoApi = new ConsumoApi();
+		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=81c2fe9f");
+//		System.out.println(json);
+//		json = consumoApi.obterDados("https://coffee.alexflipnote.dev/random.json");
+		System.out.println(json);
+
+	}
+}
---
+++ screenmatch/src/main/java/br/com/Isabela01vSilva/screenmatch/model/DadosSerie.java
@@ -0,0 +1,8 @@
+package br.com.Isabela01vSilva.screenmatch.model;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+
+public record DadosSerie(@JsonAlias("Title") String titulo,
+                         @JsonAlias("totalSeasons") Integer totalTemporadas,
+                         @JsonAlias("imdbRating") String avaliacao) {
+}
