9196c49d4211454cf7222a0b9215803633f23b19
isabela.vsilva@bandtec.com.br <isabela.vsilva@bandtec.com.br>
2024-08-27 09:03:39 -0300

    Reprsentando séries do banco

--- screenmatch/src/main/java/br/com/Isabela01vSilva/screenmatch/controller/SerieController.java
+++ screenmatch/src/main/java/br/com/Isabela01vSilva/screenmatch/controller/SerieController.java
@@ -1,13 +1,25 @@
 package br.com.Isabela01vSilva.screenmatch.controller;
 
+import br.com.Isabela01vSilva.screenmatch.dto.SerieDTO;
+import br.com.Isabela01vSilva.screenmatch.repository.SerieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 public class SerieController {
 
+    @Autowired
+    private SerieRepository repositorio;
+
     @GetMapping("/series")
-    public String obterSeries() {
-        return "Aqui será listada as séries";
+    public List<SerieDTO> obterSeries() {
+        return repositorio.findAll()
+                .stream()
+                .map(s -> new SerieDTO(s.getId(), s.getTitulo(), s.getTotalTemporadas(), s.getAvaliacao(), s.getGenero(), s.getAtores(), s.getPoster(), s.getSinopse()))
+                .collect(Collectors.toList());
     }
 }
